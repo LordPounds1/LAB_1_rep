b74453211ec090ad0c11e9994b6f70e1fb635353
LordPounds1 <91436904+LordPounds1@users.noreply.github.com>
2024-02-25 17:38:29 +0600

    Initial commit

---
+++ app/src/main/java/com/example/lab_1/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.lab_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
+    Button mainButton;
+    TextView mainTextView;
+    EditText mainEditText;
+    ListView mainListView;
+    ArrayAdapter mArrayAdapter;
+    ArrayList mNameList = new ArrayList();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mainTextView = findViewById(R.id.main_textview);
+        mainTextView.setText("Set in Java!");
+
+        mainButton = findViewById(R.id.main_button);
+        mainButton.setOnClickListener(this);
+
+        mainEditText = (EditText) findViewById(R.id.main_edittext);
+        mainListView = findViewById(R.id.main_listview);
+        mArrayAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, mNameList);
+        mainListView.setAdapter(mArrayAdapter);
+
+        mainListView.setOnItemClickListener(this);
+
+    }
+
+    @Override
+
+    public void onClick(View v) {
+        mainTextView.setText(mainEditText.getText().toString()
+                + " is learning Android development!");
+        mNameList.add(mainEditText.getText().toString());
+
+        mArrayAdapter.notifyDataSetChanged();
+
+    }
+
+    @Override
+
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.d("omg android", position + ": " + mNameList.get(position));
+        mainTextView.setText(mNameList.get(position).toString()
+                + " is learning Android development!");
+
+    }
+}
